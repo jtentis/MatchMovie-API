5dbc6b32768c2056f36114c09dfce60f60869c59
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2024-05-08 10:16:40 -0300

    Ajeitando tags API

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe)
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Match Movie')
@@ -17,7 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
 
   await app.listen(3000);
 }
