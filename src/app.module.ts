df01400bb1aa171e666303b9a09f986bbff310e7
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-14 12:24:30 -0300

    Feat: adicionando endpoints de recuperação de senha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { GeolocationModule } from './geolocation/geolocation.module';
 import { GroupsModule } from './groups/groups.module';
 import { IngressoController } from './ingresso/ingresso.controller';
 import { IngressoModule } from './ingresso/ingresso.module';
@@ -12,11 +14,10 @@ import { MatchModule } from './match/match.module';
 import { MoviesModule } from './movies/movies.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { WatchedModule } from './watched/watched.module';
-import { GeolocationModule } from './geolocation/geolocation.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, MoviesModule, GroupsModule, FavoritesModule, WatchedModule, MatchModule, IngressoModule, HttpModule, GeolocationModule],
+  imports: [PrismaModule, AuthModule, MoviesModule, GroupsModule, FavoritesModule, WatchedModule, MatchModule, IngressoModule, HttpModule, GeolocationModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, IngressoController],
   providers: [AppService, IngressoService],
 })
-export class AppModule {}
+export class AppModule { }
