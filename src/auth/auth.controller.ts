b923b28891d9361b91a4d54596892b26572e883f
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-16 02:57:39 -0300

    Feat: adicionando auths para swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 //src/auth/auth.controller.ts
 
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthEntity } from './entity/auth.entity';
@@ -13,16 +13,19 @@ export class AuthController {
 
   @Post('login')
   @ApiOkResponse({ type: AuthEntity })
+  @ApiOperation({ summary: 'Fazer login'})
   login(@Body() { email, password }: LoginDto) {
     return this.authService.login(email, password);
   }
 
   @Post('request-password-reset')
+  @ApiOperation({ summary: 'Solicitar redefinição de senha'})
   async requestPasswordReset(@Body('email') email: string) {
     return this.authService.requestPasswordReset(email);
   }
 
   @Post('reset-password')
+  @ApiOperation({ summary: 'Resetar senha'})
   async resetPassword(@Body('token') token: string, @Body('newPassword') newPassword: string) {
     return this.authService.resetPassword(token, newPassword);
   }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
+import * as basicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +13,17 @@ async function bootstrap() {
 
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  app.use(
+    ['/api/docs', '/api/docs-json'],
+    basicAuth({
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASS },
+      challenge: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Match Movie')
+    .setDescription('Documentação da API do Match Movie')
     .addTag('auth')
     .addTag('users')
     .addTag('groups')
@@ -23,11 +33,11 @@ async function bootstrap() {
     .addTag('geolocation')
     .addTag('ingresso')
     .addTag('match')
-    .setVersion('0.1')
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
